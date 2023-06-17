3e6e7e268e307b13c6e07e640d52b40b028cb605
Pradeep4592 <31234869+Pradeep4592@users.noreply.github.com>
2023-06-17 14:16:29 +0530

    initial commit

---
+++ src/test/java/com/psgroup/SDS/SdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psgroup.SDS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
